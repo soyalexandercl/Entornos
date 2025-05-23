3180c2ca956f0736b427b4bf6e62cfcf49f88bf2
Jhon Alexander Practica Git <alexcardonal24@gmail.com>
2025-05-22 19:17:05 -0500

    Añadir cuenta en secundaria1

---
+++ CuentaBancaria.java
@@ -0,0 +1,13 @@
+class TestCuentaBancaria
+{
+	public static void main(String args[])
+	{
+		CuentaBancaria cuenta = new CuentaBancaria("1234521","Director Banco",100,0);
+		CuentaBancaria cuentaNueva = new CuentaBancaria("0123456789","Nombre Apellidos",1000,5);
+		System.out.println("Saldo = " + cuenta.getSaldo());
+		cuenta.reintegro(50);
+		cuenta.ingreso(100);
+		System.out.println("Saldo: " + cuenta.getSaldo());
+	}
+}
+
